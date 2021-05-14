00eebcadd17ac39602db82897193400ec9983664
Francisco Florentino de Sousa Filho <ffsf.filho@gmail.com>
2021-05-14 20:28:26 -0300

    Projeto finalizado

--- src/controller.mjs
+++ src/controller.mjs
@@ -1,17 +1,30 @@
 export default class Controller{
-  constructor({ view }){
+  constructor({ view, media, recorder}){
     this.view = view
+    this.media = media
+    this.recorder = recorder
   }
+
   static initialize(dependencies){
     const instance = new Controller(dependencies)
     return instance._init()
   }
 
   _init(){
-    this.view.configureStartRecordingButton()
+    this.view.configureStartRecordingButton(this.onStartRecording.bind(this))
+    this.view.configureStopRecordingButton(this.onStopRecording.bind(this))
   }
-
+  
   async onStartRecording() {
-    console.log('iniciou a gravação')
+    const audioStream = await this.media.getAudio()
+    this.recorder.startRecording(audioStream)
+  }
+  
+  async onStopRecording() {
+    this.recorder.stopRecording()
+    setTimeout(() => {
+      const audioUrl = this.recorder.getRecordingURL()
+      this.view.playAudio(audioUrl)
+    });
   }
 }
--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,14 @@
-console.log("carregou")
+import Controller from "./controller.mjs";
+import Media from "./util/media.mjs"
+import Recorder from "./util/recorder.mjs";
+import View from "./view.mjs";
+
+const view = new View()
+const media = new Media()
+const recorder = new Recorder()
+
+Controller.initialize({
+  view,
+  media,
+  recorder
+})
